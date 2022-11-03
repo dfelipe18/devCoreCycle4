6c7062ea1a67feb1d91a3e5869cf6a03f1f97c6a
Filleral Mahecha <filleral19@gmail.com>
2022-11-02 21:33:47 -0500

    cambiesitos

--- devcore-app/src/components/ProductItem.jsx
+++ devcore-app/src/components/ProductItem.jsx
@@ -32,13 +32,6 @@ const ProductItem = ({data, addToCart})=> {
             <p>
                 An exploration into the truck's polarising design
             </p>
-            <div className="user">
-                <img src="https://yt3.ggpht.com/a/AGF-l7-0J1G0Ue0mcZMw-99kMeVuBmRxiPjyvIYONg=s900-c-k-c0xffffffff-no-rj-mo" alt="user" />
-                <div className="user-info">
-                    <h5>July Dec</h5>
-                    <small>2h ago</small>
-                </div>
-            </div>
         </div>
     </div>
 </div>
