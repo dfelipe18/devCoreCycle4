c46c0192d2cfc6c387602810f41760678bb0d878
Filleral Mahecha <filleral19@gmail.com>
2022-11-05 16:08:01 -0500

    desparece product item y cart item

--- devcore-app/src/components/ShoppingCart.jsx
+++ devcore-app/src/components/ShoppingCart.jsx
@@ -1,17 +1,21 @@
 import { useReducer } from "react";
-import React from "react";
+import { useState, useContext } from "react";
 import {
   shoppingInitialState,
   shoppingReducer,
 } from "../reducers/shoppingReducer";
+import React from "react";
 import ProductItem from "./ProductItem";
 import "../App.css";
 import AppNavBar from "./page/AppNavBar";
 import CartItem from "./CartItem";
 import { TYPES } from "../actions/shoppingActions";
 import { useGetUserAuth } from "../utilities/hooks/useGetUserAuth";
+import { DataContext } from "../utilities/hooks/DataContext";
 
 const ShoppingCart = () => {
+  const {dataCart, setDataCart, dataProducts, setDataProducts} = useContext(DataContext);
+  consolelog(dataProducts)
   const userAuth = useGetUserAuth();
   const [state, dispatch] = useReducer(shoppingReducer, shoppingInitialState);
 
@@ -45,7 +49,8 @@ const ShoppingCart = () => {
               data={product}
               addToCart={addToCart}
             />
-          ))}
+          ))
+          }
         </article>
         {userAuth.role !== undefined && userAuth.role !== "administradores" && (
           <div>
@@ -62,3 +67,51 @@ const ShoppingCart = () => {
   );
 };
 export default ShoppingCart;
+
+// import React from "react";
+// import "../Product.css";
+// import { useGetUserAuth } from "../utilities/hooks/useGetUserAuth";
+// const ProductItem = ({data, addToCart})=> {
+//     const userAuth = useGetUserAuth();
+//     let {id, name,price, image} = data;
+//     return (
+//     <div className="container">
+//     <div className="card">
+//         <div className="card-header">
+//             <img src={image} alt="rover" />
+//         </div>
+//         <div className="card-body">
+//             <span className="tag tag-teal">{name}</span>
+//             <h4>
+//             ${price}.00 
+//             </h4>
+//             <p>
+//                 An exploration into the truck's polarising design
+//                 <button className="custom-btn btn-12" onClick={() => addToCart(id)}><span>siuuuu!</span><span>Comprar</span></button>
+//             </p>
+//         </div>
+//     </div>
+// </div>
+//     );
+// };
+
+// export default ProductItem;
+
+// import React from "react";
+// import "../Cart.css";
+// const CartItem = ({data,delFromCart}) => {
+// let {id, name, price, quantity, image} = data;
+
+//     return (
+//         <div style ={{borderBottom: 'Thin solid gary'}}>
+//                 <img className="image1" src={image}></img>
+//             <h4>{name}</h4>
+//             <h5>${price}.00 x {quantity} =${price * quantity}.00 </h5>
+//             <button className="custom-btn btn-6" onClick={() => delFromCart(id)}>Eliminar uno</button>
+//             <br/>
+//             <button className="custom-btn btn-6" onClick={() => delFromCart(id, true)}>Eliminar todos</button>
+//         </div>
+//     );
+// };
+
+// export default CartItem;
